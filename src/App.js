f78ec86ba8863016315df3a33b080fe6937d820b
jsephora <joey@sephoradesign.com>
2025-03-24 16:23:00 +1100

    new background

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import './App.css'
-import background from './Assets/dark-triangles.png'
-import logo from './Assets/sdLogo-portrait.png'
+// import background from './Assets/pattern.png'
+// import logo from './Assets/sdLogo-superFaded.png'
 
 function App() {
   return (
-    <div
-      className='App'
-      style={{
-        backgroundImage: `url(${background})`,
-        height: '100vh',
-        // marginTop: '0px',
-      }}
-    >
+    <div className='App'>
       <div className='centerContent'>
-        <img src={logo} alt='sephora design logo'
-        style={{width: '500'}}/>
-        {/* <h1
+        <h1
           style={{
             color: 'white',
           }}
         >
           Sephora Design is under construction
-        </h1> */}
+        </h1>
       </div>
     </div>
   )
